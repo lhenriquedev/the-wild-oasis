7da23c737886e2b28fb46d0cfdf11c9b8792194f
Henrique <luiz@eway.dev>
2023-08-23 21:12:05 -0300

    ✨ feat: adding pagination component to the project

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -7,6 +7,7 @@ import Menus from '../../ui/Menus'
 // import Pagination from '../../ui/Pagination'
 import Empty from '../../ui/Empty'
 import { useBookings } from './useBookings'
+import { Pagination } from './../../ui/Pagination'
 
 // v2
 // Right now this is not really reusable... But we will want to use a similar table for guests as well, but with different columns. ALSO, right now we are defining these columns in BOTH the TableHeader and the BookingRow, which is not good at all. Instead, it would be much better to simply pass the columns into the Table, and the table would give access to the columns to both the header and row. So how can we do that? Well we can again use a compound component! We don't HAVE to do it like this, there's a million ways to implement a table, also without CSS Grid, but this is what I chose
@@ -55,6 +56,10 @@ export function BookingTable() {
             <BookingRow key={booking.id} booking={booking} />
           )}
         />
+
+        <Table.Footer>
+          <Pagination count={45} />
+        </Table.Footer>
       </Table>
     </Menus>
   )
